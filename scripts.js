d5a0e1f7c718ad949f9156cd5dbc737a6fc577d6
ayzaqwash <isaac.wachira@student.moringaschool.com>
2021-04-16 18:23:22 +0300

    added meesage feedback alert

--- scripts.js
+++ scripts.js
@@ -30,3 +30,20 @@ $(document).ready(function () {
         $("#product").hide()
     });
 });
+
+$(document).ready(function(){
+    $("form#feedback").submit(function(event){
+      event.preventDefault();
+      var name = $("input#IDENTITY").val();
+      var email = $("input#MAIL").val();
+      var message = $("textarea#comment").val();
+      if ($("input#IDENTITY").val() && $("input#MAIL").val()){
+        alert (name + ", we have received your message. Thank you for reaching out to us.");
+      }
+      else {
+        alert("kindly enter your name and email!");
+      }
+      
+    });
+  
+  });
